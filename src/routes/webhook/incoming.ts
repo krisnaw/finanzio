829c892f7d8feaa9ed53fab3bf3b72ffd5af5229
krisna <krisna.w2010@gmail.com>
2025-12-05 09:50:56 +0800

    test webhook

--- src/routes/webhook/incoming.ts
+++ src/routes/webhook/incoming.ts
@@ -7,6 +7,16 @@ export const Route = createFileRoute('/webhook/incoming')({
         console.log(request)
         return new Response('Hello, World! from incoming ' + request.url)
       },
+      POST: async ({ request }) => {
+
+        const event = await request.json();
+
+
+        console.log(event.type)
+
+
+        return new Response('post. Hello, World! from incoming ' + request.url)
+      },
     },
   },
 })
