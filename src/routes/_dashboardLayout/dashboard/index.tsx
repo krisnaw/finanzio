c031d3ea12985f75aee12aa8df7f16d1a2de954d
krisna <krisna.w2010@gmail.com>
2025-12-13 22:23:20 +0800

    wip Webhook endpoint

--- src/routes/_dashboardLayout/dashboard/index.tsx
+++ src/routes/_dashboardLayout/dashboard/index.tsx
@@ -1,4 +1,6 @@
 import {createFileRoute} from '@tanstack/react-router'
+import {useSuspenseQuery} from "@tanstack/react-query";
+import {getTransactions} from "@/serverActions/transactionActions.ts";
 
 export const Route = createFileRoute('/_dashboardLayout/dashboard/')({
   component: RouteComponent,
@@ -6,5 +8,11 @@ export const Route = createFileRoute('/_dashboardLayout/dashboard/')({
 
 
 function RouteComponent() {
+  const { data }  = useSuspenseQuery({
+    queryKey: ['getTransactions'],
+    queryFn: () => getTransactions()
+  })
+
+  console.log(data)
   return <div>Hello "/dashboard/"!</div>
 }
