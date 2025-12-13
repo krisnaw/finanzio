8efca56487d96454431bc68a7f1cf0cfae8afd14
krisna <krisna.w2010@gmail.com>
2025-12-13 22:20:47 +0800

    wip Webhook endpoint

--- src/routes/_dashboardLayout/dashboard/index.tsx
+++ src/routes/_dashboardLayout/dashboard/index.tsx
@@ -1,16 +1,10 @@
 import {createFileRoute} from '@tanstack/react-router'
-import {getTransactions} from "@/serverActions/transactionActions.ts";
 
 export const Route = createFileRoute('/_dashboardLayout/dashboard/')({
-  loader: async () => {
-    return await getTransactions()
-  },
   component: RouteComponent,
 })
 
 
 function RouteComponent() {
-  const posts = Route.useLoaderData()
-  console.log(posts)
   return <div>Hello "/dashboard/"!</div>
 }
