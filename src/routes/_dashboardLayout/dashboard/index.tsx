bb49393ee05ab2d0fe5d8081f1e290c517f9e2b6
krisna <krisna.w2010@gmail.com>
2025-12-14 08:08:12 +0800

    wip data fetching

--- src/routes/_dashboardLayout/dashboard/index.tsx
+++ src/routes/_dashboardLayout/dashboard/index.tsx
@@ -1,22 +1,20 @@
 import {createFileRoute} from '@tanstack/react-router'
-import {useSuspenseQuery} from "@tanstack/react-query";
-import {getTransactions} from "@/serverActions/transactionActions.ts";
 
 export const Route = createFileRoute('/_dashboardLayout/dashboard/')({
   component: RouteComponent,
 })
 
-
 function RouteComponent() {
-  const { data }  = useSuspenseQuery({
-    queryKey: ['getTransactions'],
-    queryFn: () => getTransactions()
-  })
-
-  console.log(data)
+  // const { data }  = useSuspenseQuery({
+  //   queryKey: ['getTransactions'],
+  //   queryFn: () => getTransactions()
+  // })
+  //
+  // console.log(data)
   return (
     <div>
-      {data?.length >= 1 && data.map((item) => <div>{item.id}</div>)}
+      Dashboard
+      {/*{data?.length >= 1 && data.map((item) => <div>{item.id}</div>)}*/}
     </div>
   )
 }
